55c25f37049b2bb0f9ea156a3c10871535e866a2
Im-Rises <quentin-morel88@hotmail.com>
2022-07-25 22:09:56 +0200

    Correcting overflow issue

--- src/si_arcade/mmu.rs
+++ src/si_arcade/mmu.rs
@@ -22,36 +22,6 @@ impl Mmu {
             memory: [0; MEMORY_SIZE],
             // memory: vec![0; MEMORY_SIZE],
         };
-        // let image = std::fs::read("TST8080.COM").ok().unwrap();
-        // let mut i = 0;
-        // for n in image {
-        //     mmu.memory[0x100 + i] = n;
-        //     i += 1;
-        // }
-
-        // let mut file = BufReader::new(File::open("TST8080.COM"));
-        // file.read_exact(&mut mmu.memory[0x100..(0x100 + 0x7FF)])
-        //     .expect("TODO: panic message");
-
-        // let mut f = File::open("TST8080.COM")?;
-        //
-        // // read exactly 10 bytes
-        // let size = f.read(&mut mmu.memory[0x100..]);
-        // Ok(());
-
-        // io::copy()
-
-        // mmu.memory[0x100..] = *fs::read("TST8080.COM").expect("TODO: panic message");
-
-        // let mut file = std::fs::File::open(&Path::new("TST8080.COM"));
-        // let number = file.rea;
-
-        // let f = File::open("TST8080.COM")?;
-        // let f = BufReader::new(f);
-        //
-        // for line in f.lines() {
-        //     println!("{}", line.unwrap());
-        // }
 
         let h = include_bytes!("invaders.h");
         let g = include_bytes!("invaders.g");
@@ -62,10 +32,6 @@ impl Mmu {
         mmu.memory[0x800..(h.len() + 0x800)].copy_from_slice(g);
         mmu.memory[0x1000..(h.len() + 0x1000)].copy_from_slice(f);
         mmu.memory[0x1800..(h.len() + 0x1800)].copy_from_slice(e);
-
-        for i in mmu.memory {
-            println!("{}", i);
-        }
 
         mmu
     }
