dd60cf8a835ea7c9850c2af38f1a5e89de7f757e
Im-Rises <quentin-morel88@hotmail.com>
2022-07-29 16:20:05 +0200

    Modified code structure

--- src/si_arcade.rs
+++ src/si_arcade.rs
@@ -1,10 +1,23 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
+use sdl2::render::{CanvasBuilder, Texture, WindowCanvas};
+use sdl2::video::Window;
+use sdl2::{Sdl, VideoSubsystem};
+
+use crate::binary_lib::get_bit;
+
 mod cpu;
 mod inputs_outputs;
 mod mmu;
 mod ppu;
+
+const WINDOW_WIDTH: usize = 600;
+const WINDOW_HEIGHT: usize = 600;
 
 const INTERRUPT_VBLANK_COUNTER: usize = cpu::CLOCK_FREQUENCY / ppu::SCREEN_FREQUENCY;
 const INTERRUPT_MIDDLE_VBLANK: usize = INTERRUPT_VBLANK_COUNTER / 2;
@@ -13,24 +26,26 @@ pub struct SpaceInvadersArcade {
     cpu: cpu::Cpu,
     mmu: Rc<RefCell<mmu::Mmu>>,
     ppu: ppu::Ppu,
-    // inputs: inputs::Imputs,
+    canvas: WindowCanvas,
+    sdl_context: Sdl,
 }
 
 impl SpaceInvadersArcade {
     pub fn new() -> SpaceInvadersArcade {
         let mmu_init = Rc::new(RefCell::new(mmu::Mmu::new()));
+        let video = SpaceInvadersArcade::init_video().unwrap();
         SpaceInvadersArcade {
             cpu: cpu::Cpu::new(&mmu_init, 0),
             mmu: Rc::clone(&mmu_init),
             ppu: ppu::Ppu::new(&mmu_init),
-            // inputs: inputs::new()
+            canvas: video.0,
+            sdl_context: video.1,
         }
     }
     pub fn start(&mut self) {
-        self.ppu.start_video();
         let mut frequency_counter: usize = 0;
         let mut last_frequency_counter: usize = 0;
-        while self.ppu.get_window_active().unwrap() {
+        while self.get_window_active().unwrap() {
             self.cpu.clock();
             frequency_counter += 1;
             if self.cpu.get_inte() {
@@ -40,7 +55,8 @@ impl SpaceInvadersArcade {
                 if frequency_counter >= INTERRUPT_VBLANK_COUNTER {
                     cpu::interrupts::interrupt(&mut self.cpu, 2);
                     frequency_counter = 0;
-                    self.ppu.clock().expect("Error : Panic happend in PPU");
+                    self.ppu.clock();
+                    self.update_screen().expect("TODO: panic message");
                 }
             } else {
                 frequency_counter = 0;
@@ -58,4 +74,64 @@ impl SpaceInvadersArcade {
     fn clock_ppu(&self) {}
 
     fn handle_inputs(&self) {}
+
+    /*-------------------SDL2 Video and Inputs---------------------*/
+
+    fn init_video() -> Result<(WindowCanvas, Sdl), String> {
+        let sdl_context = sdl2::init()?;
+        let video_subsystem = sdl_context.video()?;
+
+        let window = video_subsystem
+            .window(
+                "Space Invaders Arcade Emulator",
+                WINDOW_WIDTH as u32,
+                WINDOW_HEIGHT as u32,
+            )
+            .position_centered()
+            .resizable()
+            // .hidden()
+            .opengl()
+            .build()
+            .map_err(|e| e.to_string())?;
+
+        let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+        canvas
+            .set_logical_size(WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32)
+            .expect("TODO: panic message");
+        Ok((canvas, sdl_context))
+    }
+
+    fn update_screen(&mut self) -> Result<(), String> {
+        let texture_creator = self.canvas.texture_creator();
+        let mut texture = texture_creator
+            .create_texture_streaming(
+                PixelFormatEnum::RGB24,
+                ppu::SCREEN_WIDTH as u32,
+                ppu::SCREEN_HEIGHT as u32,
+            )
+            .map_err(|e| e.to_string())?;
+        texture
+            .update(None, self.ppu.get_screen(), ppu::SCREEN_WIDTH * 3)
+            .expect("TODO: panic message");
+
+        self.canvas.copy_ex(&texture, None, None, -90.0, None, false, false)?;
+        self.canvas.present();
+        Ok(())
+    }
+
+    fn get_window_active(&self) -> Result<bool, String> {
+        let mut event_pump = self.sdl_context.event_pump()?;
+        let mut window_active = true;
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => window_active = false,
+                _ => window_active = true,
+            }
+        }
+        Ok(window_active)
+    }
 }
--- src/si_arcade/ppu.rs
+++ src/si_arcade/ppu.rs
@@ -15,62 +15,23 @@ use crate::binary_lib::get_bit;
 use crate::si_arcade::mmu::Mmu;
 
 pub const SCREEN_FREQUENCY: usize = 60;
-const SCREEN_WIDTH: usize = 256;
-const SCREEN_HEIGHT: usize = 224;
-
-const WINDOW_WIDTH: usize = 600;
-const WINDOW_HEIGHT: usize = 600;
+pub const SCREEN_WIDTH: usize = 256;
+pub const SCREEN_HEIGHT: usize = 224;
 
 pub struct Ppu {
     mmu: Rc<RefCell<Mmu>>,
     screen: [u8; SCREEN_WIDTH * SCREEN_HEIGHT * 3],
-    canvas: WindowCanvas,
-    sdl_context: Sdl,
 }
-
-//https://github.com/Rust-SDL2/rust-sdl2/blob/master/examples/renderer-texture.rs
 
 impl Ppu {
     pub fn new(mmu: &Rc<RefCell<Mmu>>) -> Ppu {
-        let video = Ppu::init_video().unwrap();
-
         Ppu {
             mmu: Rc::clone(&mmu),
             screen: [0; SCREEN_WIDTH * SCREEN_HEIGHT * 3],
-            canvas: video.0,
-            sdl_context: video.1,
         }
     }
 
-    fn init_video() -> Result<(WindowCanvas, Sdl), String> {
-        let sdl_context = sdl2::init()?;
-        let video_subsystem = sdl_context.video()?;
-
-        let window = video_subsystem
-            .window(
-                "Space Invaders Arcade Emulator",
-                WINDOW_WIDTH as u32,
-                WINDOW_HEIGHT as u32,
-            )
-            .position_centered()
-            .resizable()
-            // .hidden()
-            .opengl()
-            .build()
-            .map_err(|e| e.to_string())?;
-
-        let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-        canvas
-            .set_logical_size(WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32)
-            .expect("TODO: panic message");
-        Ok((canvas, sdl_context))
-    }
-
-    pub fn start_video(&mut self) {
-        // self.window.show();
-    }
-
-    pub fn clock(&mut self) -> Result<(), String> {
+    pub fn clock(&mut self) {
         let mut index: usize = 0;
         for data in self.mmu.borrow().get_vram() {
             for bit in 0..8 {
@@ -86,33 +47,9 @@ impl Ppu {
                 index += 3;
             }
         }
-
-        let texture_creator = self.canvas.texture_creator();
-        let mut texture = texture_creator
-            .create_texture_streaming(PixelFormatEnum::RGB24, (SCREEN_WIDTH) as u32, SCREEN_HEIGHT as u32)
-            .map_err(|e| e.to_string())?;
-        texture
-            .update(None, &self.screen, SCREEN_WIDTH * 3)
-            .expect("TODO: panic message");
-
-        self.canvas.copy_ex(&texture, None, None, -90.0, None, false, false)?;
-        self.canvas.present();
-        Ok(())
     }
 
-    pub fn get_window_active(&self) -> Result<bool, String> {
-        let mut event_pump = self.sdl_context.event_pump()?;
-        let mut window_active = true;
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => window_active = false,
-                _ => window_active = true,
-            }
-        }
-        Ok(window_active)
+    pub fn get_screen(&self) -> &[u8; SCREEN_WIDTH * SCREEN_HEIGHT * 3] {
+        &self.screen
     }
 }
